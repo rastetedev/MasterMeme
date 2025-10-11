0f975a7c047cbfff345b1244d30b1bc0db86a83e
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-10-11 16:34:23 -0500

    style: Set dark status bar for edge-to-edge
    Modify the `enableEdgeToEdge` call in `MainActivity` to apply a dark status bar style.
    The status bar's color is now set to `PrimaryContainer` to match the app's theme when the edge-to-edge interface is enabled.

--- app/src/main/java/com/raulastete/mastermeme/MainActivity.kt
+++ app/src/main/java/com/raulastete/mastermeme/MainActivity.kt
@@ -2,17 +2,21 @@ package com.raulastete.mastermeme
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.raulastete.mastermeme.navigation.NavGraph
 import com.raulastete.mastermeme.ui.theme.MasterMemeTheme
+import com.raulastete.mastermeme.ui.theme.PrimaryContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(PrimaryContainer.value.toInt())
+        )
         setContent {
             MasterMemeTheme {
                 NavGraph()
